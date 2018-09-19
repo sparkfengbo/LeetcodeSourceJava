97b4b67cb87b88f62eec97a99edd67fd30cf54a7
fengbo <fengbo04@baidu.com>
2018-09-19 20:26:34 +0800

    3无重复字符的最长子串 好题呀

---
+++ src/LeetCode3.java
@@ -0,0 +1,130 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * 无重复字符的最长子串
+ *
+ * 给定一个字符串，找出不含有重复字符的最长子串的长度。
+ *
+ * 示例 1:
+ *
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 无重复字符的最长子串是 "abc"，其长度为 3。
+ * 示例 2:
+ *
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 无重复字符的最长子串是 "b"，其长度为 1。
+ * 示例 3:
+ *
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 无重复字符的最长子串是 "wke"，其长度为 3。
+ *      请注意，答案必须是一个子串，"pwke" 是一个子序列 而不是子串。
+ */
+public class LeetCode3 {
+
+    public static void main(String[] args) {
+        //3
+//        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+//        //1
+//        System.out.println(lengthOfLongestSubstring("bbbbb"));
+//        //3
+//        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        //2
+        System.out.println(lengthOfLongestSubstring("abba"));
+    }
+
+
+    /**
+     * 双指针解法，复杂度O(n)
+     *
+     * 思路
+     *
+     * a   b   c   c   d   e   e     长度 1
+     *
+     * i,j
+     *
+     * a   b   c   c   d   e   e     长度 2
+     *
+     * i   j
+     *
+     * a   b   c   c   d   e   e     长度 3
+     *
+     * i       j
+     *
+     * a   b   c   c   d   e   e     发现重复字符，i移动，j = i + 1
+     *
+     * i           j
+     *
+     * a   b   c   c   d   e   e
+     *
+     *             i   j
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        int length = s.length();
+
+        if (length == 0) {
+            return 0;
+        }
+
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        int maxLength = 0;
+        for (int left = 0 , right = 0; right < length; right++) {
+            if (hashMap.containsKey(s.charAt(right))) {
+                left =  Math.max(left, hashMap.get(s.charAt(right)) + 1);
+            }
+            hashMap.put(s.charAt(right), right);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+
+    /**
+     * 暴力解法， O(n3)
+     *
+     * 极端测试用例会超时
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring1(String s) {
+        int length = s.length();
+
+        if (length == 0) {
+            return 0;
+        }
+
+        int window = length;
+
+        for (int i = window; i > 0; i--) {
+            for (int j = 0; j + i <= length; j++) {
+                boolean isMaxSub = true;
+                String sub = s.substring(j, j + i);
+
+                HashSet<Character> set = new HashSet<>();
+                for (int k = 0; k < sub.length(); k++) {
+                    if (set.contains(sub.charAt(k))) {
+                        isMaxSub = false;
+                        break;
+                    } else {
+                        set.add(sub.charAt(k));
+                    }
+                }
+
+                if (isMaxSub) {
+                    return i;
+                }
+            }
+        }
+
+        return 1;
+    }
+}
