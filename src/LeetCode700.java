9096d48fc599f1738429cddde093803f5294f316
fengbo <fengbo04@baidu.com>
2018-10-13 14:26:06 +0800

    700二叉搜索树中的搜索

---
+++ src/LeetCode700.java
@@ -0,0 +1,52 @@
+/**
+ * 二叉搜索树中的搜索
+ *
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ *
+ * 例如，
+ *
+ * 给定二叉搜索树:
+ *
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ *
+ * 和值: 2
+ * 你应该返回如下子树:
+ *
+ *       2
+ *      / \
+ *     1   3
+ * 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+ */
+public class LeetCode700 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        TreeNode result = searchBST(root, 2);
+
+
+        System.out.println();
+    }
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left,val);
+        } else {
+            return searchBST(root.right,val);
+        }
+    }
+}
