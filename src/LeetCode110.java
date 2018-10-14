7c921874c33ed39226dee8b07fa7cea6bfa3899f
fengbo <fengbo04@baidu.com>
2018-10-14 16:50:20 +0800

    110平衡二叉树

---
+++ src/LeetCode110.java
@@ -0,0 +1,84 @@
+/**
+ * 平衡二叉树
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ *
+ * 本题中，一棵高度平衡二叉树定义为：
+ *
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ *
+ * 示例 1:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回 true 。
+ *
+ * 示例 2:
+ *
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ *
+ *        1
+ *       / \
+ *      2   2
+ *     / \
+ *    3   3
+ *   / \
+ *  4   4
+ * 返回 false 。
+ */
+public class LeetCode110 {
+
+    public static void main(String[] args) {
+        LeetCode110 leetCode110 = new LeetCode110();
+
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(9);
+        node.right = new TreeNode(20);
+        node.right.left = new TreeNode(15);
+        node.right.right = new TreeNode(7);
+
+
+        TreeNode node2 = new TreeNode(1);
+        node2.left = new TreeNode(2);
+        node2.right = new TreeNode(2);
+        node2.left.left = new TreeNode(3);
+        node2.left.right = new TreeNode(3);
+
+        node2.left.left.left = new TreeNode(4);
+        node2.left.left.right = new TreeNode(4);
+
+
+        System.out.println(leetCode110.isBalanced(node));
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        int leftDep = maxDepth(root.left);
+        int rightDep = maxDepth(root.right);
+
+        if (Math.abs(leftDep - rightDep) > 1) {
+            return false;
+        } else {
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+}
