a35c150c6c4564af9eb42f1bd39a3da5e4e8644d
fengbo <fengbo04@baidu.com>
2018-10-14 16:59:12 +0800

    606根据二叉树创建字符串

---
+++ src/LeetCode606.java
@@ -0,0 +1,77 @@
+/**
+ * 根据二叉树创建字符串
+ *
+ * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+ *
+ * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+ *
+ * 示例 1:
+ *
+ * 输入: 二叉树: [1,2,3,4]
+ *        1
+ *      /   \
+ *     2     3
+ *    /
+ *   4
+ *
+ * 输出: "1(2(4))(3)"
+ *
+ * 解释: 原本将是“1(2(4)())(3())”，
+ * 在你省略所有不必要的空括号对之后，
+ * 它将是“1(2(4))(3)”。
+ * 示例 2:
+ *
+ * 输入: 二叉树: [1,2,3,null,4]
+ *        1
+ *      /   \
+ *     2     3
+ *      \
+ *       4
+ *
+ * 输出: "1(2()(4))(3)"
+ *
+ * 解释: 和第一个示例相似，
+ * 除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+ */
+public class LeetCode606 {
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+        root1.left.left = new TreeNode(4);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(3);
+        root2.left.right = new TreeNode(4);
+
+        System.out.println(tree2str(root1));
+        System.out.println(tree2str(root2));
+
+    }
+
+    public static String tree2str(TreeNode t) {
+        if (t == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(t.val);
+        if (t.left != null) {
+            sb.append("(");
+            sb.append(tree2str(t.left));
+            sb.append(")");
+        } else if (t.right != null) {
+            sb.append("()");
+        }
+
+        if (t.right != null) {
+            sb.append("(");
+            sb.append(tree2str(t.right));
+            sb.append(")");
+        }
+
+        return sb.toString();
+    }
+}
